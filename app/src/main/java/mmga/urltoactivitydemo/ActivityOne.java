ae73f4ea641a5aaa1c73b878fc362e5dc24f81b6
bianyunpeng <bianyunpeng@cloudist.cc>
2017-06-14 09:37:57 +0800

    通过Query传参数

--- app/src/main/java/mmga/urltoactivitydemo/ActivityOne.java
+++ app/src/main/java/mmga/urltoactivitydemo/ActivityOne.java
@@ -10,5 +10,6 @@ public class ActivityOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+        setTitle("Activity One");
     }
 }
--- app/src/main/java/mmga/urltoactivitydemo/ActivityTwo.java
+++ app/src/main/java/mmga/urltoactivitydemo/ActivityTwo.java
@@ -1,8 +1,13 @@
 package mmga.urltoactivitydemo;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
 
 public class ActivityTwo extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class ActivityTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+        setTitle("Activity Two");
+
+        TextView textId = (TextView) findViewById(R.id.text_id);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Uri data = intent.getData();
+            String id = data.getQueryParameter("id");
+            textId.setText(MessageFormat.format("id = {0}", id));
+        }
     }
 }
